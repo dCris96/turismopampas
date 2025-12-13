bfc88afc58452080993ec8a97945b4722314284d
Cristian <dcris3344@gmail.com>
2025-12-13 11:53:48 -0500

    Ordenando ramas

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,50 @@ import taza from "@/public/taza.png";
 import humo1 from "@/public/humo1.png";
 import humo2 from "@/public/humo2.png";
 import hoja1 from "@/public/hoja1.png";
+import hoja2 from "@/public/hoja2.png";
+import hoja3 from "@/public/hoja3.png";
+import rama from "@/public/rama.png";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.hojas}>
-          <Image src={hoja1} alt="logo de la marca" />
+          <Image src={hoja1} alt="imagen de una hoja" />
+        </div>
+        <div className={styles.hojas}>
+          <Image src={hoja3} alt="imagen de una hoja" />
         </div>
         <div className={styles.cont_logo}>
           <Image src={logo} alt="logo de la marca" />
         </div>
+
         <div className={styles.cont_main}>
           <div className={styles.imagen}>
             <Image src={humo1} alt="imagen de humo1" className={styles.humo} />
-            <Image src={humo2} alt="imagen de humo2" className={styles.humo} />
             <Image
-              src={taza}
-              alt="imagen de taza"
-              className={styles.foto_taza}
+              src={humo2}
+              alt="imagen de humo2"
+              className={styles.humo}
+              loading="eager"
             />
+            <div className={styles.cont_imagen}>
+              <Image
+                src={taza}
+                alt="imagen de taza"
+                className={styles.foto_taza}
+                loading="eager"
+              />
+            </div>
+
+            <div className={styles.rama}>
+              <Image
+                src={rama}
+                alt="imagen de rama"
+                className={styles.foto_rama}
+                loading="eager"
+              />
+            </div>
           </div>
           <div className={styles.textos}>
             <h1>Infusiones Andinas</h1>
