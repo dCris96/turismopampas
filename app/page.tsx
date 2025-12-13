4db7a5f963ea4ac167c03af5f7d5a563c255f360
Cristian <dcris3344@gmail.com>
2025-12-13 00:04:13 -0500

    Agregando las hojas

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,29 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import logo from "@/public/logo.png";
 import taza from "@/public/taza.png";
+import humo1 from "@/public/humo1.png";
+import humo2 from "@/public/humo2.png";
+import hoja1 from "@/public/hoja1.png";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div className={styles.hojas}>
+          <Image src={hoja1} alt="logo de la marca" />
+        </div>
         <div className={styles.cont_logo}>
           <Image src={logo} alt="logo de la marca" />
         </div>
         <div className={styles.cont_main}>
           <div className={styles.imagen}>
-            <Image src={taza} alt="imagen de taza" />
+            <Image src={humo1} alt="imagen de humo1" className={styles.humo} />
+            <Image src={humo2} alt="imagen de humo2" className={styles.humo} />
+            <Image
+              src={taza}
+              alt="imagen de taza"
+              className={styles.foto_taza}
+            />
           </div>
           <div className={styles.textos}>
             <h1>Infusiones Andinas</h1>
